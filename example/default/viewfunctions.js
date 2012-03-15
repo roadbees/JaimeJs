4e5000b77c01e13b23cbde19bf78fde8ac07b236
Roadbees <nicolas@roadbees.de>
2012-03-15 16:15:17 +0100

    changed the engine, new jquery on method used

--- example/default/viewfunctions.js
+++ example/default/viewfunctions.js
@@ -3,6 +3,10 @@ var ViewFunctions = {
 	"makeItRed" : function() {		
 		$("#first-text").css('color','red');		
 	},
+
+	"makeItBlue" : function() {		
+		$("#first-text").css('color','blue');		
+	},
 	
 	"twitterCallback" : function(data) {
 		$('#twitter-target').html(JSON.stringify(data));
